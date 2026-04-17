6fcbe263c3533e60af5536e1cdf4b51ff0c784b1
CarlosJaBR <carjabo12@gmail.com>
2026-04-16 23:13:07 -0500

    refactor: edit methods on LoginStepDefinition

--- src/test/java/com/serenity/stepDefinitions/LoginStepDefinition.java
+++ src/test/java/com/serenity/stepDefinitions/LoginStepDefinition.java
@@ -1,5 +1,6 @@
 package com.serenity.stepDefinitions;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -7,14 +8,12 @@ import io.cucumber.java.en.When;
 public class LoginStepDefinition {
     @Given("user open the browser")
     public void userOpenTheBrowser() {
-        throw new io.cucumber.java.PendingException();
     }
     @When("the user send the credentials")
-    public void theUserSendTheCredentials(io.cucumber.datatable.DataTable dataTable) {
-        throw new io.cucumber.java.PendingException();
+    public void theUserSendTheCredentials(DataTable dataTable) {
+
     }
     @Then("the user can see the message {string}")
     public void theUserCanSeeTheMessage(String string) {
-        throw new io.cucumber.java.PendingException();
     }
 }
