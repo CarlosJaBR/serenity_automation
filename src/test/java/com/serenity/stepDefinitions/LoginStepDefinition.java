8b2f799646f298174685c0a3133d2e0f3450029c
CarlosJaBR <carjabo12@gmail.com>
2026-04-16 23:11:26 -0500

    feat: add methods on LoginStepDefinition

--- src/test/java/com/serenity/stepDefinitions/LoginStepDefinition.java
+++ src/test/java/com/serenity/stepDefinitions/LoginStepDefinition.java
@@ -1,4 +1,20 @@
 package com.serenity.stepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginStepDefinition {
+    @Given("user open the browser")
+    public void userOpenTheBrowser() {
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user send the credentials")
+    public void theUserSendTheCredentials(io.cucumber.datatable.DataTable dataTable) {
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user can see the message {string}")
+    public void theUserCanSeeTheMessage(String string) {
+        throw new io.cucumber.java.PendingException();
+    }
 }
