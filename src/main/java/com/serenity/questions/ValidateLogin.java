e0e8d0bd6ca9be77e601ac57f602b744c4ac37fd
CarlosJaBR <carjabo12@gmail.com>
2026-04-20 00:35:09 -0500

    feat: add logic to validate the cases in ValidateLogin and LoginStepDefinition classes

--- src/main/java/com/serenity/questions/ValidateLogin.java
+++ src/main/java/com/serenity/questions/ValidateLogin.java
@@ -1,10 +1,16 @@
 package com.serenity.questions;
 
+import com.serenity.ui.HomeUI;
 import com.serenity.ui.LoginUI;
-import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
 
 public class ValidateLogin {
 
+    public static Question<Boolean> dashboardIsVisible() {
+        return actor -> HomeUI.LBL_DASHBOARD.resolveFor(actor).isVisible();
+    }
 
+    public static Question<Boolean> loginAlertIsVisible() {
+        return actor -> LoginUI.ALERT.resolveFor(actor).isVisible();
+    }
 }
--- src/test/java/com/serenity/stepDefinitions/LoginStepDefinition.java
+++ src/test/java/com/serenity/stepDefinitions/LoginStepDefinition.java
@@ -1,13 +1,21 @@
 package com.serenity.stepDefinitions;
 
+import com.serenity.questions.ValidateLogin;
 import com.serenity.tasks.LoginTask;
 import com.serenity.tasks.OpenBrowserTask;
+import com.serenity.ui.HomeUI;
+import com.serenity.ui.LoginUI;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+import static org.hamcrest.Matchers.is;
 
 public class LoginStepDefinition {
     @Given("user open the browser")
@@ -23,6 +31,39 @@ public class LoginStepDefinition {
         );
     }
     @Then("the user can see the message {string}")
-    public void theUserCanSeeTheMessage(String string) {
+    public void theUserCanSeeTheMessage(String expectedMessage) {
+        if (isSuccessfulLogin(expectedMessage)) {
+            theActorInTheSpotlight().attemptsTo(
+                    WaitUntil.the(HomeUI.LBL_DASHBOARD, WebElementStateMatchers.isVisible())
+                            .forNoMoreThan(10).seconds()
+            );
+            theActorInTheSpotlight().should(
+                    seeThat(ValidateLogin.dashboardIsVisible(), is(true))
+            );
+            return;
+        }
+
+        if (isInvalidLogin(expectedMessage)) {
+            theActorInTheSpotlight().attemptsTo(
+                    WaitUntil.the(LoginUI.ALERT, WebElementStateMatchers.isVisible())
+                            .forNoMoreThan(10).seconds()
+            );
+            theActorInTheSpotlight().should(
+                    seeThat(ValidateLogin.loginAlertIsVisible(), is(true))
+            );
+            return;
+        }
+
+        throw new IllegalArgumentException("Valor esperado no soportado: " + expectedMessage);
+    }
+
+    private boolean isSuccessfulLogin(String expectedMessage) {
+        return "Dashboard".equalsIgnoreCase(expectedMessage)
+                || "Tablero".equalsIgnoreCase(expectedMessage);
+    }
+
+    private boolean isInvalidLogin(String expectedMessage) {
+        return "Alert".equalsIgnoreCase(expectedMessage)
+                || "Alerta".equalsIgnoreCase(expectedMessage);
     }
 }
