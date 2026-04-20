2b660645110ddfa487864d30bc8375a34f6344e4
CarlosJaBR <carjabo12@gmail.com>
2026-04-19 21:05:35 -0500

    feat: edit userOpenTheBrowser method on LoginStepDefinition

--- src/test/java/com/serenity/stepDefinitions/LoginStepDefinition.java
+++ src/test/java/com/serenity/stepDefinitions/LoginStepDefinition.java
@@ -1,13 +1,19 @@
 package com.serenity.stepDefinitions;
 
+import com.serenity.tasks.OpenBrowserTask;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+
 public class LoginStepDefinition {
     @Given("user open the browser")
     public void userOpenTheBrowser() {
+        theActorInTheSpotlight().wasAbleTo(
+                OpenBrowserTask.openBrowser()
+        );
     }
     @When("the user send the credentials")
     public void theUserSendTheCredentials(DataTable dataTable) {
