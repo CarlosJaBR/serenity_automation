c94ad69bc3d65a3b22d9774b2d288bba1cefbd42
CarlosJaBR <carjabo12@gmail.com>
2026-04-20 14:17:17 -0500

    feat: add SPN_MODULE and SPN_ORDER selectors

--- src/main/java/com/serenity/ui/DashboardUI.java
+++ src/main/java/com/serenity/ui/DashboardUI.java
@@ -6,4 +6,11 @@ public class DashboardUI {
     public static final Target LBL_DASHBOARD=
             Target.the("Label dashboard")
                     .locatedBy("//section[@class='content-header' and .//h1[normalize-space()='Tablero']]");
+
+    public static final Target SPN_MODULE=
+            Target.the("span module Northwind")
+                    .locatedBy("//span[text()='Northwind']");
+    public static final Target SPN_ORDER=
+            Target.the("span order")
+                    .locatedBy("//span[text()='Pedidos']");
 }
