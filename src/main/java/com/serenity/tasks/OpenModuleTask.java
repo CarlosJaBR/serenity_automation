7db8c7ba9f54ccc666ae8a816c4187f63c28e14f
CarlosJaBR <carjabo12@gmail.com>
2026-04-20 20:49:01 -0500

    feat: add logic in OpenModuleTask class

--- src/main/java/com/serenity/tasks/OpenModuleTask.java
+++ src/main/java/com/serenity/tasks/OpenModuleTask.java
@@ -1,11 +1,30 @@
 package com.serenity.tasks;
 
+import com.serenity.ui.DashboardUI;
+import io.cucumber.datatable.DataTable;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
 
 public class OpenModuleTask implements Task {
+
+    private final String module;
+    private final String option;
+
+    public OpenModuleTask(String module, String option) {
+        this.module = module;
+        this.option = option;
+    }
     @Override
     public <T extends Actor> void performAs(T actor) {
-
+        actor.attemptsTo(
+                Click.on(DashboardUI.SPN_MODULE.of(module)),
+                Click.on(DashboardUI.SPN_ORDER.of(option))
+        );
+    }
+    public static OpenModuleTask openModule(String module,String option){
+        return instrumented(OpenModuleTask.class,module,option);
     }
 }
