34c3ab48e8bcea7c9a134fa44ef8d23b9d4a45f6
CarlosJaBR <carjabo12@gmail.com>
2026-04-20 21:22:33 -0500

    feat: add LBL_CUSTOMER

--- src/main/java/com/serenity/ui/NewOrderUI.java
+++ src/main/java/com/serenity/ui/NewOrderUI.java
@@ -1,4 +1,11 @@
 package com.serenity.ui;
 
+import net.serenitybdd.screenplay.targets.Target;
+
 public class NewOrderUI {
+
+    public final Target LBL_CUSTOMER=
+            Target.the("Label customer")
+                    .locatedBy("//div[contains(@class,'select2-result-label') and normalize-space()='Alfreds Futterkiste [ALFKI]']");
+
 }
--- src/test/java/com/serenity/stepDefinitions/NewOrderStepDefinition.java
+++ src/test/java/com/serenity/stepDefinitions/NewOrderStepDefinition.java
@@ -1,48 +1,35 @@
 package com.serenity.stepDefinitions;
 
+import com.serenity.tasks.OpenCreateOrder;
 import com.serenity.tasks.OpenModuleTask;
-import com.serenity.ui.OrderUI;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import net.serenitybdd.screenplay.actions.Click;
 
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class NewOrderStepDefinition {
     @When("the user navigates to the {string} section in the {string} module")
-    public void theUserNavigatesToTheSectionInTheModule(String section, String module) {
+    public void theUserNavigatesToTheSectionInTheModule(String string, String string2) {
         theActorInTheSpotlight().attemptsTo(
-                OpenModuleTask.openModule(section, module)
+                OpenModuleTask.openModule(string,string2)
         );
     }
-
     @When("clicks on the {string} button")
-    public void clicksOnTheButton(String buttonName) {
-        switch (buttonName.trim().toLowerCase()) {
-            case "new order":
-            case "nuevo pedido":
-                theActorInTheSpotlight().attemptsTo(
-                        Click.on(OrderUI.SPN_NEW_ORDER)
-                );
-                break;
-            default:
-                throw new IllegalArgumentException("Button not supported in this step: " + buttonName);
-        }
+    public void clicksOnTheButton(String string) {
+        theActorInTheSpotlight().attemptsTo(
+                OpenCreateOrder.openCreateOrder(string)
+        );
     }
-
     @When("fills out the form with the following information:")
     public void fillsOutTheFormWithTheFollowingInformation(DataTable dataTable) {
     }
-
     @When("adds a product to the order detail:")
     public void addsAProductToTheOrderDetail(DataTable dataTable) {
     }
-
     @When("saves the record")
     public void savesTheRecord() {
     }
-
     @Then("the system should the order created successfully")
     public void theSystemShouldTheOrderCreatedSuccessfully() {
     }
