721ca1b6f5420821c094a115437d3169b17f9ee5
CarlosJaBR <carjabo12@gmail.com>
2026-04-20 21:26:22 -0500

    feat: add CreateNewOrderTask class

---
+++ src/main/java/com/serenity/tasks/CreateNewOrderTask.java
@@ -0,0 +1,12 @@
+package com.serenity.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+
+public class CreateNewOrderTask implements Task {
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+
+    }
+}
